2f6080b39ef7933d64c04ff1bb376c5d2da28688
vleadadmin <admin@vlabs.ac.in>
2025-07-20 12:56:06 +0000

    https://virtual-labs.github.io/virtual-labs/exp-scales-dei click on the link to test your code.

---
+++ simulation/images/js/exp3.js
@@ -0,0 +1,488 @@
+// VARIABLES
+
+// DROPMENUS, PENCILS, COMPASS AND INSTRUCTIONS
+const assemblyBox = document.querySelector("#assembly");
+const statementBox = document.querySelector("#exp1-statement");
+const instruction1Step3 = document.querySelector("#instructions-step3");
+const stepNumber = document.querySelector(".step-number");
+
+const comp1Step3 = document.querySelector("#compass1-step3");
+const comp2Step3 = document.querySelector("#compass2-step3");
+const comp3Step3 = document.querySelector("#compass3-step3");
+
+const pencil1Step3 = document.querySelector("#pencil1-step3");
+const pencil2Step3 = document.querySelector("#pencil2-step3");
+const pencil3Step3 = document.querySelector("#pencil3-step3");
+const pencil4Step3 = document.querySelector("#pencil4-step3");
+const pencil5Step3 = document.querySelector("#pencil5-step3");
+const pencil6Step3 = document.querySelector("#pencil6-step3");
+const pencil7Step3 = document.querySelector("#pencil7-step3");
+const pencil8Step3 = document.querySelector("#pencil8-step3");
+const pencil9Step3 = document.querySelector("#pencil9-step3");
+const pencil10Step3 = document.querySelector("#pencil10-step3");
+const pencil11Step3 = document.querySelector("#pencil11-step3");
+
+const dropMenu1Step3 = document.querySelector("#dropMenu1-step3");
+const dropMenu2Step3 = document.querySelector("#dropMenu2-step3");
+const dropMenu3Step3 = document.querySelector("#dropMenu3-step3");
+const dropMenu4Step3 = document.querySelector("#dropMenu4-step3");
+const dropMenu5Step3 = document.querySelector("#dropMenu5-step3");
+const dropMenu6Step3 = document.querySelector("#dropMenu6-step3");
+const dropMenu7Step3 = document.querySelector("#dropMenu7-step3");
+const dropMenu8Step3 = document.querySelector("#dropMenu8-step3");
+const dropMenu9Step3 = document.querySelector("#dropMenu9-step3");
+const dropMenu10Step3 = document.querySelector("#dropMenu10-step3");
+
+// IMAGES
+const png1Step3 = document.querySelector("#png1-step3");
+const png2Step3 = document.querySelector("#png2-step3");
+const png3Step3 = document.querySelector("#png3-step3");
+const png4Step3 = document.querySelector("#png4-step3");
+const png5Step3 = document.querySelector("#png5-step3");
+const png6Step3 = document.querySelector("#png6-step3");
+const png7Step3 = document.querySelector("#png7-step3");
+const png8Step3 = document.querySelector("#png8-step3");
+const png9Step3 = document.querySelector("#png9-step3");
+const png10Step3 = document.querySelector("#png10-step3");
+const png11Step3 = document.querySelector("#png11-step3");
+const png12Step3 = document.querySelector("#png12-step3");
+const png13Step3 = document.querySelector("#png13-step3");
+const png14Step3 = document.querySelector("#png14-step3");
+const png15Step3 = document.querySelector("#png15-step3");
+const finalPng = document.querySelector("#finalPng-step3");
+
+// GIFS
+const gif1Step3 = document.querySelector("#gif1-step3");
+const gif2Step3 = document.querySelector("#gif2-step3");
+const gif3Step3 = document.querySelector("#gif3-step3");
+const gif4Step3 = document.querySelector("#gif4-step3");
+const gif5Step3 = document.querySelector("#gif5-step3");
+const gif6Step3 = document.querySelector("#gif6-step3");
+const gif7Step3 = document.querySelector("#gif7-step3");
+const gif8Step3 = document.querySelector("#gif8-step3");
+const gif9Step3 = document.querySelector("#gif9-step3");
+const gif10Step3 = document.querySelector("#gif10-step3");
+const gif11Step3 = document.querySelector("#gif11-step3");
+const gif12Step3 = document.querySelector("#gif12-step3");
+const gif13Step3 = document.querySelector("#gif13-step3");
+const gif14Step3 = document.querySelector("#gif14-step3");
+
+// TRANSBOXES
+const trans1Step3 = document.querySelector("#trans1-step3");
+const trans2Step3 = document.querySelector("#trans2-step3");
+const trans3Step3 = document.querySelector("#trans3-step3");
+const trans4Step3 = document.querySelector("#trans4-step3");
+const trans5Step3 = document.querySelector("#trans5-step3");
+const trans6Step3 = document.querySelector("#trans6-step3");
+const trans7Step3 = document.querySelector("#trans7-step3");
+const trans8Step3 = document.querySelector("#trans8-step3");
+const trans9Step3 = document.querySelector("#trans9-step3");
+const trans10Step3 = document.querySelector("#trans10-step3");
+const trans11Step3 = document.querySelector("#trans11-step3");
+const trans12Step3 = document.querySelector("#trans12-step3");
+const trans13Step3 = document.querySelector("#trans13-step3");
+
+// ARROWS
+const arr1Step3 = document.querySelector("#arr1-step3");
+const arr2Step3 = document.querySelector("#arr2-step3");
+const arr3Step3 = document.querySelector("#arr3-step3");
+const arr4Step3 = document.querySelector("#arr4-step3");
+const arr5Step3 = document.querySelector("#arr5-step3");
+const arr6Step3 = document.querySelector("#arr6-step3");
+const arr7Step3 = document.querySelector("#arr7-step3");
+const arr8Step3 = document.querySelector("#arr8-step3");
+
+// NEXT BUTTONS 
+const arrNext = document.querySelector(".arr-next");
+const nextToFinalPng = document.querySelector("#nextToFinalPng");
+const nextBtnExp1 = document.querySelector("#nextToExpListFromExp1");
+
+
+// once dom loaded then trans1Step3 will get activated.
+addEventListener("DOMContentLoaded", () =>{
+    trans1Step3.style.visibility = "visible";
+    arr1Step3.style.visibility = "visible";
+    png1Step3.style.visibility = "visible";
+});
+
+trans1Step3.addEventListener("click",() => {
+    trans1Step3.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+    dropMenu1Step3.style.visibility = "visible";
+});
+
+
+// dropmenu 1 changes
+
+dropMenu1Step3.addEventListener("change",(evt)=>{
+    let selectedValue = dropMenu1Step3.value;
+    if(selectedValue === "hPencil"){
+        dropMenu1Step3.style.visibility = "hidden";
+        pencil1Step3.style.visibility = "visible";
+        arr2Step3.style.visibility = "visible";
+    }else{
+        alert("Please select H Pencil");
+    }
+});
+
+// when first H Pencil was clicked 
+
+pencil1Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    png1Step3.style.visibility = "hidden";
+    arr2Step3.style.visibility = "hidden";
+    
+    gif1Step3.style.visibility = "visible";
+
+    setTimeout((evt) => {
+        gif1Step3.style.visibility = "hidden";
+        png2Step3.style.visibility = "visible";
+
+        instruction1Step3.innerText = "Now make a rectangle.";
+        stepNumber.innerText = "4";
+
+        arr3Step3.style.visibility = "visible";
+        pencil2Step3.style.visibility = "visible";
+    }, 8600)
+});
+
+
+pencil2Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr3Step3.style.visibility = "hidden";
+    png2Step3.style.visibility = "hidden";
+
+    gif2Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+    png3Step3.style.visibility = "visible";
+    gif2Step3.style.visibility = "hidden";
+    instruction1Step3.innerText = "Divide the rectangle into 4 equal parts.";
+    stepNumber.innerText = "5";
+
+    arr3Step3.style.visibility = "visible";
+    pencil3Step3.style.visibility = "visible";
+    }, 21500);
+});
+
+pencil3Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr3Step3.style.visibility = "hidden";
+    png3Step3.style.visibility = "hidden";
+
+    gif3Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+    png4Step3.style.visibility = "visible";
+    gif3Step3.style.visibility = "hidden";
+    instruction1Step3.innerText = "Select 3H pencil and draw a line inclined to the rectangle.";
+    stepNumber.innerText = "6";
+
+    arr1Step3.style.visibility = "visible";
+    trans2Step3.style.visibility = "visible";
+    }, 33910);
+
+});
+
+trans2Step3.addEventListener("click", ()=> {
+    trans2Step3.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+    dropMenu2Step3.style.visibility = "visible";
+    dropMenu2Step3.value = "default";
+});
+
+dropMenu2Step3.addEventListener("change",() => {
+    let selectedValue = dropMenu2Step3.value;
+    if(selectedValue === "3hPencil"){
+        dropMenu2Step3.style.visibility = "hidden";
+        pencil4Step3.style.visibility = "visible";
+        arr3Step3.style.visibility = "visible";
+    }else{
+        alert("Please select 3H Pencil");
+    }
+});
+
+pencil4Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr3Step3.style.visibility = "hidden";
+    png4Step3.style.visibility = "hidden";
+
+    gif4Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        png5Step3.style.visibility = "visible";
+        gif4Step3.style.visibility = "hidden";
+        instruction1Step3.innerText = "Select the Compass and devide the line into 10 equal parts.";
+        stepNumber.innerText = "7";
+
+        arr4Step3.style.visibility = "visible";
+        trans3Step3.style.visibility = "visible";
+        }, 10000);
+
+});
+
+trans3Step3.addEventListener("click", () => {
+    trans3Step3.style.visibility = "hidden";
+    arr4Step3.style.visibility = "hidden";
+    arr5Step3.style.visibility = "visible";
+    comp1Step3.style.visibility = "visible";
+});
+
+comp1Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr5Step3.style.visibility = "hidden";
+    png5Step3.style.visibility = "hidden";
+
+    gif5Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        gif5Step3.style.visibility = "hidden";
+        png6Step3.style.visibility = "visible";
+        arr1Step3.style.visibility = "visible";
+        instruction1Step3.innerText = "Select 3H pencil and draw parallel lines.";
+        stepNumber.innerText = "8";
+        trans4Step3.style.visibility = "visible";
+    }, 45080);
+});
+
+trans4Step3.addEventListener("click", ()=> {
+    trans4Step3.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+    dropMenu3Step3.style.visibility = "visible";
+    dropMenu3Step3.value = "default";
+});
+
+dropMenu3Step3.addEventListener("change",() => {
+    let selectedValue = dropMenu3Step3.value;
+    if(selectedValue === "3hPencil"){
+        dropMenu3Step3.style.visibility = "hidden";
+        pencil5Step3.style.visibility = "visible";
+        arr3Step3.style.visibility = "visible";
+    }else{
+        alert("Please select 3H Pencil");
+    }
+});
+
+pencil5Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr3Step3.style.visibility = "hidden";
+    png6Step3.style.visibility = "hidden";
+
+    gif6Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        png7Step3.style.visibility = "visible";
+        gif6Step3.style.visibility = "hidden";
+        instruction1Step3.innerText = "Select H pencil and subdivide the division to the left of zero into subunits.";
+        stepNumber.innerText = "9";
+
+        arr1Step3.style.visibility = "visible";
+        trans6Step3.style.visibility = "visible";
+        }, 50000);
+
+});
+
+trans6Step3.addEventListener("click", ()=> {
+    trans6Step3.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+    dropMenu5Step3.style.visibility = "visible";
+    dropMenu5Step3.value = "default";
+});
+
+dropMenu5Step3.addEventListener("change",() => {
+    let selectedValue = dropMenu5Step3.value;
+    if(selectedValue === "hPencil"){
+        dropMenu5Step3.style.visibility = "hidden";
+        pencil6Step3.style.visibility = "visible";
+        arr3Step3.style.visibility = "visible";
+    }else{
+        alert("Please select H Pencil");
+    }
+});
+
+pencil6Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr3Step3.style.visibility = "hidden";
+    png7Step3.style.visibility = "hidden";
+
+    gif7Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        png8Step3.style.visibility = "visible";
+        gif7Step3.style.visibility = "hidden";
+        instruction1Step3.innerText = "Select 3H pencil and take a length of line starting from zero equal to 9 m.s.d .";
+        stepNumber.innerText = "10";
+
+        arr3Step3.style.visibility = "visible";
+        pencil7Step3.style.visibility = "visible";
+    }, 40150)
+});
+
+pencil7Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr3Step3.style.visibility = "hidden";
+    png8Step3.style.visibility = "hidden";
+
+    gif8Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+    png9Step3.style.visibility = "visible";
+    gif8Step3.style.visibility = "hidden";
+    instruction1Step3.innerText = "Select 3H pencil and make an inclined line.";
+    stepNumber.innerText = "11";
+
+    arr1Step3.style.visibility = "visible";
+    trans7Step3.style.visibility = "visible";
+    }, 24640);
+});
+
+trans7Step3.addEventListener("click", ()=> {
+    trans7Step3.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+    dropMenu6Step3.style.visibility = "visible";
+    dropMenu6Step3.value = "default";
+});
+
+dropMenu6Step3.addEventListener("change",() => {
+    let selectedValue = dropMenu6Step3.value;
+    if(selectedValue === "3hPencil"){
+        dropMenu6Step3.style.visibility = "hidden";
+        pencil8Step3.style.visibility = "visible";
+        arr3Step3.style.visibility = "visible";
+    }else{
+        alert("Please select 3H Pencil");
+    }
+});
+
+pencil8Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr3Step3.style.visibility = "hidden";
+    png9Step3.style.visibility = "hidden";
+
+    gif9Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        png10Step3.style.visibility = "visible";
+        gif9Step3.style.visibility = "hidden";
+        instruction1Step3.innerText = "Select compass and divide the line into 10 equal parts.";
+        stepNumber.innerText = "12";
+
+        arr4Step3.style.visibility = "visible";
+        trans8Step3.style.visibility = "visible";
+    }, 9950)
+});
+
+trans8Step3.addEventListener("click", () => {
+    trans8Step3.style.visibility = "hidden";
+    arr4Step3.style.visibility = "hidden";
+    arr5Step3.style.visibility = "visible";
+    comp3Step3.style.visibility = "visible";
+});
+
+comp3Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr5Step3.style.visibility = "hidden";
+    png10Step3.style.visibility = "hidden";
+
+    gif10Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        gif10Step3.style.visibility = "hidden";
+        png11Step3.style.visibility = "visible";
+        arr1Step3.style.visibility = "visible";
+        instruction1Step3.innerText = "Now, Select 3H pencil and draw parallel lines.";
+        stepNumber.innerText = "13";
+        trans9Step3.style.visibility = "visible";
+    }, 28210);
+});
+
+trans9Step3.addEventListener("click", ()=> {
+    trans9Step3.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+    dropMenu7Step3.style.visibility = "visible";
+    dropMenu7Step3.value = "default";
+});
+
+dropMenu7Step3.addEventListener("change",() => {
+    let selectedValue = dropMenu7Step3.value;
+    if(selectedValue === "3hPencil"){
+        dropMenu7Step3.style.visibility = "hidden";
+        pencil9Step3.style.visibility = "visible";
+        arr3Step3.style.visibility = "visible";
+    }else{
+        alert("Please select 3H Pencil");
+    }
+});
+
+pencil9Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr3Step3.style.visibility = "hidden";
+    png11Step3.style.visibility = "hidden";
+
+    gif11Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        png12Step3.style.visibility = "visible";
+        gif11Step3.style.visibility = "hidden";
+        instruction1Step3.innerText = "Select H pencil and subdivide the division into 10 equal parts.";
+        stepNumber.innerText = "14";
+
+        arr1Step3.style.visibility = "visible";
+        trans10Step3.style.visibility = "visible";
+    }, 40800)
+});
+
+trans10Step3.addEventListener("click", ()=> {
+    trans10Step3.style.visibility = "hidden";
+    arr1Step3.style.visibility = "hidden";
+    dropMenu8Step3.style.visibility = "visible";
+    dropMenu8Step3.value = "default";
+});
+
+dropMenu8Step3.addEventListener("change",() => {
+    let selectedValue = dropMenu8Step3.value;
+    if(selectedValue === "hPencil"){
+        dropMenu8Step3.style.visibility = "hidden";
+        pencil10Step3.style.visibility = "visible";
+        arr3Step3.style.visibility = "visible";
+    }else{
+        alert("Please select H Pencil");
+    }
+});
+
+pencil10Step3.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arr3Step3.style.visibility = "hidden";
+    png12Step3.style.visibility = "hidden";
+
+    gif12Step3.style.visibility = "visible";
+
+    setTimeout(() => {
+        gif12Step3.style.visibility = "hidden";
+        png13Step3.style.visibility = "visible";
+        }, 39850);
+
+    setTimeout(() => {
+        nextToFinalPng.style.visibility = "visible";
+        arrNext.style.visibility = "visible";
+        }, 41000)
+});
+
+nextToFinalPng.addEventListener("click", (evt) => {
+    evt.target.style.visibility = "hidden";
+    arrNext.style.visibility = "hidden";
+    assemblyBox.style.visibility = "hidden";
+    statementBox.style.visibility = "hidden";
+
+    finalPng.style.visibility = "visible";
+    finalPng.style.top = "80px";
+
+    setTimeout(()=> {
+        nextBtnExp1.style.visibility = "visible";
+        arrNext.style.visibility = "visible";
+    }, 1500);
+})
+
+nextBtnExp1.addEventListener("click", () => {
+    parent.location = `../exp-list.html`;   // both of these addresses will work.
+})
---
+++ theory.md
@@ -0,0 +1,140 @@
+### Theory 
+
+Whether working on projects related to civil engineering, mechanical designs or electronic schematics, the ability to interpret and apply scales is a foundational skill. Scales ensure that drawings accurately represent real-world dimensions, providing a universal language for engineers to communicate design specifications effectively. 
+It is possible to produce drawings of small items that are the same size as the objects they depict. A drawing of 100 mm in length could represent a pen that is 100 mm long. Full-scale drawings are those that are drawn to the exact size of the things. For such drawings, standard full-size scales are employed.
+#### Scale
+A scale is essentially the ratio of the linear dimensions of an element in a drawing to the actual dimensions of the same element in the real object.
+The scales generally used for general engineering drawings are shown in table below-
+
+
+#### Scale Reference Table
+
+| Reducing Scales | 1:2 | 1:5 | 1:10 |
+| ---------------- | :--- | --- | ---- |
+|                 | 1:20 | 1:50 | 1:100 | 
+|                 | 1:200 | 1:500 | 1:1000 | 
+|                 | 1:2000 | 1:5000 | 1:10000 | 
+| Enlarging Scales |  50:1 | 20:1 | 10:1 |
+|                 | 5:1 | 2:1 |     |
+| Full Size Scales |    |     | 1:1 |
+
+
+Some situations might not allow for the preparation of full-size drawings. As a result, they are depicted proportionately bigger or smaller. A decreasing scale (1: 5) is used when drawings are made smaller than the actual size of the items (such as enormous equipment, buildings, bridges, etc.). Small machine parts, mathematical apparatus, timepieces, and other items are portrayed in drawings that are larger than they actually are. It is claimed that these are drawn with an expanding scale (5: 1).
+
+The scales can be expressed in the following three ways:
+1.	Engineer's Scale: In this instance, the relationship between the object's actual dimension and its dimension on the design is expressed numerically in the manner, for example, 10 mm = 5 m.
+2.	Graphical scale: Graphical scale: The drawing itself has the scale drawn on it. The engineer's scale may decrease and results may become inaccurate as the drawing ages. With graphical scale, this is not the case since the scale will likewise reduce if the artwork does. Because of this, survey maps frequently employ the graphical scale.
+3.	Representative fraction: The Representative Fraction (abbreviated R.F.) is the ratio of the object's length as drawn to its actual length on the drawing. 
+R.F.=  (Length of the drawing)/(Actual length of object)
+	3.1. When a 1 cm long line in a drawing represents 1 metre length of the object, the R.F. is equal to 1cm/ 1 m  = 1 / (1 x 100 cm)= 1/100 and the scale of the drawing will be   1 : 100 or 1/100 full size. The R.F. of a drawing is greater than unity when it is drawn on an enlarging scale.
+	3.2. For example, when a 2 mm long edge of an object is shown in drawing by a line 1cm long the R.F. is 1cm/2mm = 10mm/2mm = 5. Such a drawing is said to drawn on scale 5 : 1 or five times full-size.
+
+#### Types of Scales
+The scales used in practice are classified as under: 
+1. Plain scales
+2. Diagonal scales 
+3. Comparative scales
+4. Vernier scales
+5. Scale of chords
+
+### 1 Plain Scale
+A line that has been divided into an appropriate number of equal parts, or units, with the first unit being further divided into smaller parts, makes up a plain scale. A unit and its sub-division, or two units, are represented by plain scales.
+
+In every scale,
+1. The zero should be placed at the end of the first main division, i.e. between the unit and its sub-divisions.
+2. From the zero mark, the units should be numbered to the right and its sub-divisions to the left. 
+3. The names of the units and the sub-divisions should be stated clearly below or at the respective ends. 
+4. The name of the scale (e.g. scale, 1 : 10) or its R.F. should be mentioned below the scale.
+
+
+### 2. Diagonal Scales
+
+**Purpose**: To measure small subdivisions of a unit very accurately (such as 1/10th or 1/100th of a unit).
+
+**How it works**:  
+A main scale is drawn with equal divisions. Across these divisions, diagonals are drawn within rectangles to allow further subdivisions. This enables reading values to more decimal places by following the diagonals.
+
+**Example**: If you want to measure 1.63 cm, the value 1.6 cm is taken from the main scale, and 0.03 cm is read using the diagonal.
+
+---
+
+### 3. Comparative Scales
+
+**Purpose**: To compare two different units of measurement on the same drawing (e.g., kilometers and miles, or meters and feet).
+
+**How it works**:  
+Two parallel scales are drawn — one for each unit — starting from the same point. This allows direct comparison of corresponding values between the two units.
+
+**Example**: You can see how many miles correspond to 10 kilometers directly by looking at the comparative scale.
+
+---
+
+### 4. Vernier Scales
+
+**Purpose**: To measure values more precisely than the main scale allows (used in vernier calipers, micrometers, etc.).
+
+**How it works**:  
+A main scale is marked with standard divisions. A small, sliding vernier scale is attached, with its divisions slightly offset from the main scale. This shifting alignment allows the reading of fractions of a division.
+
+**Example**: If one main scale division = 1 mm and you want to read 1.76 mm, the vernier provides the additional precision for the 0.76 mm part.
+
+---
+
+### 5. Scale of Chords
+
+**Purpose**: To measure and construct angles using a compass when a protractor is not available.
+
+**How it works**:  
+A chord is a straight line connecting two points on a circle. The scale of chords uses the length of these chords to represent various angles. By setting a compass to the chord length for a desired angle, you can mark and draw that angle accurately.
+
+**Example**: To draw a 30° angle, set your compass to the chord length marked 30° on the scale of chords and transfer this length to your drawing.
+
+---
+
+#### Formulas for Scales
+
+---
+
+### 1️. Representative Fraction (R.F.)
+
+R.F. = Length of drawing ÷ Actual length of object
+
+---
+
+### 2️. Diagonal Scales
+
+Value of one small subdivision = Value of main division ÷ n
+
+Where:  
+n = number of parts the diagonal is divided into
+
+---
+
+### 3️. Comparative Scales
+
+Length on 1st scale ÷ Actual unit of 1st scale = Length on 2nd scale ÷ Actual unit of 2nd scale
+
+---
+
+### 4️. Vernier Scales (Least Count / Vernier Constant)
+
+Least Count (LC) = 1 MSD − 1 VSD
+
+Where:  
+- MSD = Main Scale Division  
+- VSD = Vernier Scale Division
+
+Alternate form:  
+LC = Smallest division on main scale ÷ Number of divisions on Vernier scale
+
+---
+
+### 5️. Scale of Chords
+
+Chord length = 2 × R × sin(θ ÷ 2)
+
+Where:  
+- R = Radius of the circle  
+- θ = Angle in degrees
+
+---
